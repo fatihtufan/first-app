bb6a333c06b5e72b9b9e33c1262d7e536d69e9bf
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2023-12-07 17:10:57 +0300

    koşullu render

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 
 const name = "Fatih";
 const surname = "Tufan";
+const isLoggedIn = false;
 
 function App() {
   return (
     <>
-      <h1>{`Benim Adım:${name}, Soyadım:${surname}`}</h1>
+      <h1>
+        {isLoggedIn ? `Benim Adım:${name}, Soyadım:${surname}` : `giriş yapmadınız.`}</h1> 
+
     </>
   );
 }
